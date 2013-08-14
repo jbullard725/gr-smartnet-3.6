c5b9e44dcb08844f0802155fb9299211778a7c83
Luke <lukekb@gmail.com>
2013-08-14 11:34:27 -0400

    Ready to go

--- src/python/start_hack_rf_decode_856.sh
+++ src/python/start_hack_rf_decode_856.sh
@@ -1,1 +1,1 @@
-python smartnet2logging_rtl.py -f 856207500.0 -c 856207500.0  -r 4000000 -E -5500 -v 7
+python smartnet2decode_rtl.py -f 856187500.0 -c 856207500.0  -r 2000000 -E -5500 # starting decoding
